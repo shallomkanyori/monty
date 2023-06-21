701fe7c9c927cf5b17b0c457be505fd9599f553c
Master8631 <murigipeternjogu254@gmail.com>
2023-06-21 12:14:57 +0000

    task 1e

--- instructions.c
+++ instructions.c
@@ -90,5 +90,10 @@ void pint(stack_t **stack, unsigned int line_number)
 	{
 		printf("%d\n", top->n);
 	}
+	else
+	{
+		printf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit (EXIT_FAILURE);
+	}
 
 }
