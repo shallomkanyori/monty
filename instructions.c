047a65820e0f6dcc8f9fa14f3d3ca815c805be0f
Master8631 <murigipeternjogu254@gmail.com>
2023-06-21 18:09:05 +0000

    task 2h

--- instructions.c
+++ instructions.c
@@ -101,7 +101,7 @@ void pop(stack_t **stack, unsigned int line_number)
 
 	if (stack == NULL || *stack == NULL)
 	{
-		fprintf(stderr, "L%u: can't pop, stack empty\n",);
+		fprintf(stderr, "L<line_number>: can't pop an empty stack\n", line_number);
 		my_exit(1);
 	}
 
